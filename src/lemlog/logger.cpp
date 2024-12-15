0b852b3592e8deb146eefd22ccee585634517d84
Liam Teale <liamteale6@gmail.com>
2024-12-15 13:22:01 -0800

    add log helper class

--- src/lemlog/logger.cpp
+++ src/lemlog/logger.cpp
@@ -5,6 +5,16 @@ namespace logger {
 
 static std::list<Sink*> sinks;
 
+Helper::Helper(std::string topic)
+    : m_topic(topic) {}
+
+void Helper::log(Level level, std::string message) {
+    // send a message to every sink
+    for (Sink* const sink : sinks) {
+        sink->send(level, m_topic, message);
+    }
+}
+
 Sink::Sink() { sinks.push_back(this); }
 
 Sink::~Sink() { sinks.remove(this); }
