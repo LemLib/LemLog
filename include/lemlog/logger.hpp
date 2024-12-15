14999b25ec13bd13d42d8eb0ba262606a666f8fb
Liam Teale <liamteale6@gmail.com>
2024-12-15 11:55:23 -0800

    rename lemlog.hpp to logger.hpp

--- include/lemlog/logger.hpp
+++ include/lemlog/logger.hpp
@@ -5,6 +5,8 @@
 namespace logger {
 class Sink {
     public:
+        Sink();
         virtual void send(std::string topic, std::string message);
+        virtual ~Sink();
 };
 } // namespace logger
