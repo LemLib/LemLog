4f8de0f556a30ee0fd5b44228823f690193884ed
Liam Teale <liamteale6@gmail.com>
2024-12-17 17:45:42 -0800

    implement static log function

--- src/lemlog/logger/logger.cpp
+++ src/lemlog/logger/logger.cpp
@@ -5,14 +5,18 @@ namespace logger {
 
 static std::list<Sink*> sinks;
 
+static void log(Level level, std::string topic, std::string message) {
+    // send a message to every sink
+    for (Sink* const sink : sinks) {
+        sink->send(level, topic, message);
+    }
+}
+
 Helper::Helper(std::string topic)
     : m_topic(topic) {}
 
 void Helper::log(Level level, std::string message) {
-    // send a message to every sink
-    for (Sink* const sink : sinks) {
-        sink->send(level, m_topic, message);
-    }
+    logger::log(level, m_topic, message);
 }
 
 Sink::Sink() { sinks.push_back(this); }
